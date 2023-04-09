a8dab16a7fc032662fc9c81df4e1cdee16b87e26
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2023-04-10 02:04:48 +0800

    Recommit

--- pages/components/Posts.tsx
+++ pages/components/Posts.tsx
@@ -12,18 +12,19 @@ const Posts: FC<Props> = ({ items }) => {
       <div className="text-3xl font-bold">Our Products</div>
       <hr className="my-8" />
       <div className="grid grid-cols-3 gap-8">
-        {Object.entries(items).map(([key, value]) => (
-          <div key={value}>
-            <div className="rounded-2xl mb-5 shadow-lg overflow-hidden">
-              <img
-                src={`/images/posts/${key}.png`}
-                alt={value}
-                className="object-contain"
-              />
+        {items &&
+          Object.entries(items).map(([key, value]) => (
+            <div key={value}>
+              <div className="rounded-2xl mb-5 shadow-lg overflow-hidden">
+                <img
+                  src={`/images/posts/${key}.png`}
+                  alt={value}
+                  className="object-contain"
+                />
+              </div>
+              <div className="px-2 text-md font-bold">{value}</div>
             </div>
-            <div className="px-2 text-md font-bold">{value}</div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
